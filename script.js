19e642c27938e89acb9f53569c136d0d8ea5d3d5
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-12-28 15:07:29 -0600

    blueprint style

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const colored = true
+    const colored = false
 
     const target = document.getElementById('square-target')
-    if (!colored){
     target.style.border = '3px solid #f2f2f2'
-    }
     const targetRect = target.getBoundingClientRect()
     const targetCenter = {
         x: targetRect.left + targetRect.width / 2,
@@ -26,9 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }))
     
     squares.forEach((square, index) => {
-        if (!colored) {
+
             square.style.border = '3px solid #f2f2f2'
-        }
+
         square.addEventListener('mouseover', event => {
             let color = colors[index]
             if (colored) {
@@ -41,12 +39,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 squareCenter.x - targetCenter.x
             )
 
-            console.log(targetAngle)
             const targetDistance = Math.sqrt(
                 Math.pow(squareCenter.x - targetCenter.x, 2) +
                 Math.pow(squareCenter.y - targetCenter.y, 2)
             )
-            const lineLength = targetDistance - targetRect.width /4
+            const lineLength = targetDistance - targetRect.width /2
 
             const lineLeft = squareCenter.x - lineLength
             const lineTop = squareCenter.y
@@ -59,42 +56,69 @@ document.addEventListener('DOMContentLoaded', () => {
             line.style.width = `${lineLength}px`
             line.style.transformOrigin = 'top right'
             line.style.transform = `rotate(${targetAngle}rad)`
+            line.style.opacity = '1.0'
+            
+            let squareRect = square.getBoundingClientRect()
+            let back = document.getElementById('back2')
+            back.style.clipPath = `polygon(${squareRect.left}px ${squareRect.top}px, ${squareRect.right}px ${squareRect.top}px, ${squareRect.right}px ${squareRect.bottom}px, ${squareRect.left}px ${squareRect.bottom}px)`
+            
+            let back2 = document.getElementById('back3')
+            back2.style.clipPath = `polygon(${targetRect.left}px ${targetRect.top}px, ${targetRect.right}px ${targetRect.top}px, ${targetRect.right}px ${targetRect.bottom}px, ${targetRect.left}px ${targetRect.bottom}px)`
+            
+
+
+
+
             if (colored){
             line.style.background = `linear-gradient(to left, ${color} ${leftStop}px, dodgerblue ${rightStop}px)`} else {
-                line.style.borderTop = '3px dashed #f2f2f2'
+                line.style.borderTop = '3px dotted #f2f2f2'
             }
             line.style.backgroundSize = '100% 100%'
             if (colored){
             line.style.paddingTop = '3px'} else {
                 line.style.paddingTop = '0px'
             }
-            line.style.zIndex = '2'
+            line.style.zIndex = '3'
             line.style.opacity = '1'
 
             if (targetAngle > -Math.PI / 4 && targetAngle < Math.PI / 4) {
                 if (colored){
                 target.style.borderRight = `3px solid dodgerblue`}
+                else {
+                    target.style.borderRight = `3px dotted #f2f2f2`
+                }
             } else if (targetAngle > Math.PI / 4 && targetAngle < 3 * Math.PI / 4) {
                 if (colored){
-                target.style.borderBottom = `3px solid dodgerblue`}
+                target.style.borderBottom = `3px solid dodgerblue`} else {
+                    target.style.borderBottom = `3px dotted #f2f2f2`
+                }
             } else if (targetAngle < -Math.PI / 4 && targetAngle > -3 * Math.PI / 4) {
                 if (colored){
-                target.style.borderTop = `3px solid dodgerblue`}
+                target.style.borderTop = `3px solid dodgerblue`} else {
+                    target.style.borderTop = `3px dotted #f2f2f2`
+                }
             } else {
                 if (colored){
-                target.style.borderLeft = `3px solid dodgerblue`}
+                target.style.borderLeft = `3px solid dodgerblue`} else {
+                    target.style.borderLeft = `3px dotted #f2f2f2`
+                }
             }
 
         })
         square.addEventListener('mouseout', event => {
-            if (colored){
-            square.style.border = '3px dashed #f2f2f2'} else {
-                square.style.border = '3px solid #f2f2f2'
-            }
+
+            square.style.border = '3px solid #f2f2f2'
+            
             const line = document.getElementById('line')
+            line.style.opacity = '0.5'
             if (colored){
             line.style.background = 'linear-gradient(to left, #f2f2f2, #f2f2f2)'
-            target.style.border = '3px dashed #f2f2f2'}
+            target.style.border = '3px solid #f2f2f2'
+
+            } else {
+                target.style.border = '3px solid #f2f2f2'
+            }
+            
 
 
         })
